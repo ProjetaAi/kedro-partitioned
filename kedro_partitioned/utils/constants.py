6f54850ab64ec090e8f80936b7c49fb5417855c1
nickolasrm <nickolas.machado@ipiranga.ipiranga>
2022-09-01 22:49:45 +0000

    fixed ncpu and nnodes parsing from env
    Signed-off-by: nickolasrm <nickolas.machado@ipiranga.ipiranga>

--- kedro_partitioned/utils/constants.py
+++ kedro_partitioned/utils/constants.py
@@ -2,7 +2,7 @@
 import os
 
 
-MAX_WORKERS = os.environ.get('MAX_WORKERS', os.cpu_count() or 1)
+MAX_WORKERS = int(os.environ.get('MAX_WORKERS', os.cpu_count() or 1))
 """Maximum number of cpus to use in parallel."""
-MAX_NODES = os.environ.get('MAX_NODES', 1)
+MAX_NODES = int(os.environ.get('MAX_NODES', 1))
 """Maximum number of cluster computers."""
