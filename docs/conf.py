fbd9f2e17c85651246d65fa450e338a4cb573517
nickolasrm <nickolas.machado@ipiranga.ipiranga>
2022-09-02 00:26:54 +0000

    changed theme to a pypi compatible one
    Signed-off-by: nickolasrm <nickolas.machado@ipiranga.ipiranga>

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
-    'revitron_sphinx_theme',
     'sphinx_autodoc_typehints',
     'sphinx_copybutton',
 ]
@@ -101,14 +100,18 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = "revitron_sphinx_theme"
+html_theme = "sphinx_book_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'color_scheme': 'dark'
+    'single_page': True,
+}
+
+html_context = {
+    'default_mode': 'dark'
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
--- docs/index.rst
+++ docs/index.rst
@@ -3,8 +3,11 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
+Kedro Partitioned
+-----
+
 Introduction
-===========================================
+==================
 Kedro Plugin focused on extending and helping the user to process partitioned
 data.
 
